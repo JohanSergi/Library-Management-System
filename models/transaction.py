b939074b26342bdadc6b10ee567c66b28acba353
JohanSergi <sergijohan04@gmail.com>
2025-06-17 10:01:34 +0530

    Added Limit for Books taken by Customers.

--- models/transaction.py
+++ models/transaction.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields,api
 from datetime import timedelta
 from odoo.exceptions import ValidationError
 
@@ -16,16 +16,27 @@ class LibraryTransaction(models.Model):
         ('taken', 'Taken'),
         ('returned', 'Returned')
     ], string='Status', default='draft')
+    total_quantity = fields.Integer(string="Total Quantity", compute="_compute_total_quantity", store=True)
 
+    @api.depends('transaction_line_ids.quantity')
+    def _compute_total_quantity(self):
+        for record in self:
+            record.total_quantity = sum(line.quantity for line in record.transaction_line_ids)
 
     def action_take_book(self):
         for transaction in self:
             if transaction.state != 'draft':
                 raise ValidationError("Books can only be taken in Draft state.")
+            if transaction.total_quantity > 5:
+                raise ValidationError("Exceeds Limit..!")
             for line in transaction.transaction_line_ids:
                 if line.book_id.available_copies < line.quantity:
                     raise ValidationError(
                         f"Not enough copies for '{line.book_id.title}'. Available: {line.book_id.available_copies}"
+                    )
+                if transaction.customer_id.books_taken > 4:
+                    raise ValidationError(
+                        f"Exceeds the limit. Already Taken: {transaction.customer_id.books_taken} books."
                     )
                 transaction.customer_id.books_taken += line.quantity
                 line.book_id.available_copies -= line.quantity
